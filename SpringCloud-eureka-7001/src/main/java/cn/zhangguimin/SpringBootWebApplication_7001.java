23db9b707ec2e1412beffa6757b6c13833212554
test <@SONY-VAIO.mshome.net>
2018-05-16 21:37:59 +0800

    eureka注册

--- SpringCloud-eureka-7001/src/main/java/cn/zhangguimin/SpringBootWebApplication_7001.java
+++ SpringCloud-eureka-7001/src/main/java/cn/zhangguimin/SpringBootWebApplication_7001.java
@@ -2,13 +2,13 @@ package cn.zhangguimin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class SpringBootWebApplication_8001 {
+@EnableEurekaServer
+public class SpringBootWebApplication_7001 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWebApplication_8001.class, args);
+		SpringApplication.run(SpringBootWebApplication_7001.class, args);
 	}
 }
